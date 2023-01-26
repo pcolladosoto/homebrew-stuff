99e05db8f8d8ec1acefb24a86a8d390a93c70504
Pablo Collado Soto <pcolladosoto@gmx.com>
2023-01-26 10:46:34 +0100

    Add etcher cask

---
+++ Casks/etcher.rb
@@ -0,0 +1,21 @@
+cask "balenaetcher" do
+  version "1.14.3"
+  sha256 :no_check
+
+  url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
+      verified: "github.com/balena-io/etcher/"
+  name "Etcher"
+  desc "Tool to flash OS images to SD cards & USB drives"
+  homepage "https://balena.io/etcher"
+
+  app "balenaEtcher.app"
+
+  uninstall quit: "io.balena.etcher.*"
+
+  zap trash: [
+    "~/Library/Application Support/balena-etcher",
+    "~/Library/Preferences/io.balena.etcher.helper.plist",
+    "~/Library/Preferences/io.balena.etcher.plist",
+    "~/Library/Saved Application State/io.balena.etcher.savedState",
+  ]
+end
